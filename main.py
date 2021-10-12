38fe747ae5bf2116587af5716f3f1441d2b7c141
@SerhiiYanovskyi <sergejanovskij71@gmail.com>
2021-10-12 11:17:41 +0300

    HW1

--- main.py
+++ main.py
@@ -2,21 +2,15 @@ from django.core.management import execute_from_command_line
 from django.http import HttpResponse
 from django.urls import path
 from django.conf import settings
-import math
-
-import this
-from random import choice
-
-# URL
-# https://docs.python.org # /3/library/index.html path
+import importlib.util
 
 settings.configure(
     DEBUG=True,
     ROOT_URLCONF=__name__,
-    SECRET_KEY='asdf'
+    SECRET_KEY='gggg'
 )
 
-text = [x for x in dir(math) if not x.startswith('_')]
+
 TEMPLATE = """
 <!DOCTYPE html>
 <html>
@@ -31,17 +25,29 @@ TEMPLATE = """
     """
 
 
-def mod_handler(request, ):
-    links = ['<a href ="{}">{}</a><br>'.format(name,name) for name in text]
-    return HttpResponse(TEMPLATE.format(content=''.join(links)))
+def mod_handler(request, mod_name):
+    if importlib.util.find_spec(mod_name) == None:
+        return 404
+    else:
+        list = [x for x in dir(__import__(mod_name)) if not x.startswith('_')]
+        links = ['<a href ="{}">{}</a><br>'.format(name,name) for name in list]
+        return HttpResponse(TEMPLATE.format(content=''.join(links),))
 
-def obj_handler(request, mod_name, obj_name ):
-    pass
+
+def obj_handler(request, mod_name, obj_name):
+    link = [
+            '<pre style="word-wrap: break-word; white-space: pre-wrap;"> {} </pre>'
+            .format(getattr(__import__(mod_name), obj_name).__doc__)
+           ]
+    return HttpResponse(TEMPLATE.format(content=link))
+
+
+
 
 urlpatterns = [
-    path('', mod_handler ),
-    path('<mod_name>/<obj_name>', obj_handler)
-]
+               path('doc/<mod_name>', mod_handler),
+               path('doc/<mod_name>/<obj_name>', obj_handler)
+              ]
 
 
 if __name__ == '__main__':
